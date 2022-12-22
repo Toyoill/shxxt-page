1d529a94ffb58a8476568b20abe6c79b91045ff3
dnwls6102 <dnwls6102@naver.com>
2022-12-22 19:25:38 +0900

    라우팅 추가 + 헤더푸터 라우터로
    냉무

--- client/shxxt-page/src/components/UI/Footer/Footer.tsx
+++ client/shxxt-page/src/components/UI/Footer/Footer.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import styled from "styled-components";
 
 const FooterContainer = styled.div({
-  position: "fixed",
+  // position: "fixed",
+  marginTop: "3rem",
   bottom: 0,
   left: 0,
   width: "100%",
